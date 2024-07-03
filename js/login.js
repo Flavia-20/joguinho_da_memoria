800dce69c7c34a2bb5932d8fbb294e1cf00fae86
Flavia-20 <flaviamonique38@gmail.com>
2024-07-03 20:07:03 -0300

    ajuste no login

--- js/login.js
+++ js/login.js
@@ -1,36 +1,23 @@
-//pegando as teags do HTML
+// Pegando as tags do HTML
 const input = document.querySelector('.login__input');
 const button = document.querySelector('.login__button');
 const form = document.querySelector('.login-form');
 
-// const validarInput = (evento) => {
-//   validar se foi digitado uma palavra com pelomenos 3 caracteres
-//   console.log(evento.target.value); o target tras o que esta sendo digitado
-//aqui esta pegando o evento e dele pegando o target mas dá pra fazer iso direto no parâmetro da função
-// }
-
-//Assim fica penagando o valor do input direto no parâmetro, se chama destructure, eu tenho um enento e dele o target
-const validarInput = ({target})=>{
-//   validar se foi digitado uma palavra com pelomenos 3 caracteres
-    if(target.value.length > 2){
-      button.removeAttribute('disabled');
-      return;
+// Validar se foi digitado uma palavra com pelo menos 3 caracteres
+const validarInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
     }
-    button.setAttribute('disabled','');
-    
+    button.setAttribute('disabled', '');
 }
 
-const handleSubmit = (event) =>{
-  event.preventDefault();
-
-  localStorage.setItem('Player',input.value);//primeiro parametro é a chave do valor a ser salvo e o segundo vai ser o valor em si
-  window.location = 'pages/game.html';
+const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('player', input.value); // 'player' deve ser consistente
+    window.location = '../pages/game.html';
 }
 
-//adicionando evento na tag input, toda vez que o input for preenchido uma função vai ser executada
+// Adicionando evento na tag input, toda vez que o input for preenchido uma função vai ser executada
 input.addEventListener('input', validarInput);
-input.addEventListener('form', handleSubmit);
-
-
-
-
+form.addEventListener('submit', handleSubmit); // Corrigir para adicionar o evento 'submit' ao formulário
